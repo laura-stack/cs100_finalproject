a9160c98de468732ada71552ffcfb83ff92b2e1a
jshabun <jshab003@ucr.edu>
2021-05-17 22:31:56 -0700

    initial menu format

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void intro() {
+    cout << "=================================================" << endl;
+    cout << "Movie Recommender" << endl;
+    cout << "by: Laura Lopez, Joshua Shabun, William Mayuga" <<  endl;
+    cout << "(CS 100 | Spring 2021)" <<  endl;
+    cout << "=================================================" << endl;
+}
+
+void menu() {
+    cout << "Movie Recommender Menu:" << endl;
+    cout << "Enter the number of the menu option you want (i.e. 1)" << endl;
+    cout << endl;
+    cout << 
+}
+
+int main() {
+    int menuOption = 0;
+
+    intro();
+    menu();
+    
+    return 0;
+}
