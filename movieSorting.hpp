e7c788c862f4b99bbae65f2ea89ef6cb584f5c13
Aguyam1 <jumpman5.wm@gmail.com>
2021-05-18 02:12:18 -0700

    Template for movieSort class

---
+++ movieSorting.hpp
@@ -0,0 +1,19 @@
+#ifndef __MOVIESORTING_HPP__
+#define __MOVIESORTING_HPP__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "categories.hpp"
+
+using namespace std;
+
+class movieSorting(){
+	public:
+		virtual sortRefine(int var1, string var2, Base* var3)
+
+}
+
+
+#endif
