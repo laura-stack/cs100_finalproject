a8518971406c18a979cf5b76dde812a1f48f6187
Aguyam1 <jumpman5.wm@gmail.com>
2021-05-18 02:17:05 -0700

    Sets template for SortRating

---
+++ rating_sort.hpp
@@ -0,0 +1,15 @@
+#ifndef __RATING_SORT_HPP__
+#define __RATING_SORT_HPP__
+
+#include <iostream>
+#include <string>
+#include "Base.hpp"
+
+using namespace std;
+
+class ratingSort() {
+	public:
+		sortRefine(int var1, string var2, Base* var3) 
+}
+
+#endif //__RATING_SORT_HPP__
