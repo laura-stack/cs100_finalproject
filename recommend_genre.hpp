837a9edcfc4668c9b8e5219d3ee65cdcb1f9fe5b
jshabun <jshab003@ucr.edu>
2021-05-18 02:06:32 -0700

    created recommend genre subclass

---
+++ recommend_genre.hpp
@@ -0,0 +1,19 @@
+#ifndef __RECOMMEND_GENRE_HPP__
+#define __RECOMMEND_GENRE_HPP__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "recommendation.hpp"
+
+using namespace std;
+
+class Genre : public Recommendation {
+    public:
+        virtual void recommendList(int var1, Base* var2) {
+            
+        }
+};
+
+#endif //__RECOMMEND_GENRE_HPP__
