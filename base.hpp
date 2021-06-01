7c5cddaf3e85a2b8b1b37b920133d3276ca3b570
laura-stack <lhuer017@ucr.edu>
2021-05-31 22:32:12 -0700

    Base class implementation

--- base.hpp
+++ base.hpp
@@ -1,20 +1,23 @@
-
-#ifndef __BASE_HPP__
+fndef __BASE_HPP__
 #define __BASE_HPP__
-#include <vector>
+#include <vector> 
 #include <string>
-#include <iostream>
-#include <iomanip>
-
+#include <iostream> 
 
 using namespace std;
 
-class Base {
-  public:
-        //void addMovie(Base const &newMovie) = 0;
-        //void removeMovie(Base const &newMovie) = 0;
-  // virtual string getTitle() = 0;
-  // virtual void getGenre() = 0;
-   virtual void displayAllInfo() = 0;
+class Base { 
+  protected: 
+	string title;
+	vector<string> genres;
+  public: 
+	Base() : title(""), genres() {};    
+
+	Base(string title, vector<string> genres) : title(title), genres(genres){}; 
+
+	virtual string getMovieTitle() = 0; 
+	virtual void setTitle(string newTitle) = 0; 
+
 };
-#endif                      
+
+#endif
