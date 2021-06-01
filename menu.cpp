2ce8a06a6f8120df124dc95e2fc95ee0e7c89291
jshabun <jshab003@ucr.edu>
2021-05-31 23:16:39 -0700

    new changes to menu

---
+++ menu.cpp
@@ -0,0 +1,49 @@
+#include "menu.h"
+#include <iostream>
+
+using namespace std;
+
+void intro() {
+    cout << "=================================================" << endl;
+    cout << "Movie Recommender" << endl;
+    cout << "by: Laura Lopez, Joshua Shabun, William Mayuga" <<  endl;
+    cout << "(CS 100 | Spring 2021)" <<  endl;
+    cout << "=================================================" << endl;
+}
+
+void menu() {
+    cout << endl;
+    cout << "Movie Recommender Menu:" << endl;
+    cout << "Enter the number of the menu option you want (i.e. 1 or q to quit)" << endl;
+    cout << endl;
+    cout << "1. Recommend by Title" << endl;
+    cout << "2. Recommend by Genre" << endl;
+    cout << "3. Recommend by Actor/Actress" << endl;
+    cout << "4. Recommend by Rating" << endl;
+    cout << "5. Recommend by Director" << endl;
+	cout << "6. Recommend by Date"
+    cout << "Q. Quit" << endl << endl;
+    cout << "Enter option: ";
+}
+
+void option1() {
+	
+}
+
+void option2() {
+}
+
+void option3() {
+}
+
+void option4() {
+}
+
+void option5() {
+}
+
+void option6() {
+}
+
+void quit() {
+}
---
+++ menu.h
@@ -0,0 +1,18 @@
+#define __MENU_HPP__
+#
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void intro();
+void menu();
+void option1();
+void option2();
+void option3();
+void option4();
+void option5();
+void option6();
+void quit();
