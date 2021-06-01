8ad57ba397ab795efecd2da3e039d2e817ae0f40
Aguyam1 <jumpman5.wm@gmail.com>
2021-05-31 19:08:23 -0700

    adds functions to parse csv file

---
+++ Scan.hpp
@@ -0,0 +1,28 @@
+#ifndef __SCAN_HPP__
+#define __SCAN_HPP__
+
+#include <string>
+#include <vector>
+#include <map>
+#include "movie.hpp"
+#include "Strategy.hpp"
+
+using namespace std;
+
+class Strategy;
+class Scan {
+	private:
+		map<string, Movie*> movMap;
+		Strategy* filterl
+	public:
+		vector<Movie*> movies;
+		vector<Movie*> getMovies();
+		map<string, Movie*> getMovMap();
+		void setFilters(Strategy* filters);
+		void setMovies(vector<Movie*> movs);
+		void filter();
+		bool isMovie(string movName);
+		void scanMovies();
+};
+
+#endif
